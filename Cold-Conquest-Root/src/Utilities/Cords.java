7b33bda48cd068a080cc91ac8cd493e0aeacfa39
MichaelPeriod <39890684+MichaelPeriod@users.noreply.github.com>
2023-07-29 18:07:19 -0400

    Final housekeeping on code

--- Cold-Conquest-Root/src/Utilities/Cords.java
+++ Cold-Conquest-Root/src/Utilities/Cords.java
@@ -5,8 +5,9 @@ import GameComponents.GameObjects.Tiles.TileMap;
 import java.awt.*;
 
 public class Cords {
-    private final TileMap tm;
+    private final TileMap tm; //The object cords belongs to
 
+    //Initialized the cords object
     public Cords(TileMap tm){
         this.tm = tm;
 
@@ -25,8 +26,9 @@ public class Cords {
         */
     }
 
+    /*Map to world*/
+    //Converts from map coordinates to world coordinates
     public Point mapToWorld(Point pos){
-        //When placing tiles the top seems to be sub-pixel off so leaving for now but to keep an eye on
         final Point sp = tm.getStartPoint();
         final int[] size = tm.getTileBaseSize().clone();
         size[1] -= tm.getTileBaseThinkness();
@@ -36,11 +38,13 @@ public class Cords {
         );
     }
 
+    //Takes the world cords and offsets it to the top left corner
     public Point mapToWorldCorner(Point pos){
         Point toReturn = mapToWorld(pos);
         return worldCornerOffset(toReturn);
     }
 
+    //Moves the tile selection to the center of the tile
     public Point mapToWorldCenter(Point pos){
         Point toReturn = mapToWorld(pos);
         toReturn.y -= (tm.getTileBaseSize()[1] + tm.getTileBaseThinkness()) / 2;
@@ -48,25 +52,31 @@ public class Cords {
         return toReturn;
     }
 
+    /*World to map*/
+    //Takes physical cords and finds the map point closest to it
     public Point worldToMap(Point pos) {
+        //Save point as clone
         Point worldPos = new Point(pos.x, pos.y);
 
+        //Offset by world start draw point
         Point startPos = tm.getStartPoint();
         worldPos.translate(-startPos.x, -startPos.y);
 
+        //Offset by tile base
         int[] tileSize = tm.getTileBaseSize().clone();
         tileSize[1] -= tm.getTileBaseThinkness();
         tileSize[0] /= 2;
         tileSize[1] /= 2;
 
+        //CALCULATIONS
         //worldX / tileSize[0] = isoX - isoY
         //worldY / tileSize[1] = isoX + isoY
-
         //given this
         //isoX = isoY + (worldX / tileSize[0])
         //(worldY / tileSize[1] - worldX / tileSize[0]) / 2 = isoY
         //worldY / tileSize[1] - ((worldY / tileSize[1] - worldX / tileSize[0]) / 2) = isoX
 
+        //Get position
         float isoY = (worldPos.y / (float)tileSize[1] - worldPos.x / (float)tileSize[0]) / 2f;
         float isoX = worldPos.y / (float)tileSize[1] - isoY;
         return new Point(
@@ -75,14 +85,18 @@ public class Cords {
         );
     }
 
+    //Gets the nearest tile corner
     public Point worldToMapCorner(Point pos){
         return worldToMap(worldCornerOffset(pos));
     }
 
+    //Gets the nearest tile center
     public Point worldToMapCenter(Point pos){
         return worldToMap(worldToCenterOffset(pos));
     }
 
+    /*Util*/
+    //Offsets cords to corner
     private Point worldCornerOffset(Point pos){
         Point toReturn = pos;
         toReturn.x -= tm.getTileBaseSize()[0] / 2;
@@ -91,6 +105,7 @@ public class Cords {
         return toReturn;
     }
 
+    //Offsets cords to center
     private Point worldToCenterOffset(Point pos){
         return new Point(
                 pos.x,
